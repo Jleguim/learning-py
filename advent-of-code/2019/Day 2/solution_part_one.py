c62c5b90b708ebceea0de73cd64938b7f12839b2
Javier Leguizamo <jlegui745@gmail.com>
2020-10-24 22:50:40 +0000

    After a good "night" of sleep, I spotted some problems in my code.

--- advent-of-code/2019/Day 2/solution_part_one.py
+++ advent-of-code/2019/Day 2/solution_part_one.py
@@ -1,27 +1,23 @@
-separator = ','
-input = open('./input.txt', 'r').read().split(separator)
+input = [1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,9,19,23,2,23,13,27,1,27,9,31,2,31,6,35,1,5,35,39,1,10,39,43,2,43,6,47,1,10,47,51,2,6,51,55,1,5,55,59,1,59,9,63,1,13,63,67,2,6,67,71,1,5,71,75,2,6,75,79,2,79,6,83,1,13,83,87,1,9,87,91,1,9,91,95,1,5,95,99,1,5,99,103,2,13,103,107,1,6,107,111,1,9,111,115,2,6,115,119,1,13,119,123,1,123,6,127,1,127,5,131,2,10,131,135,2,135,10,139,1,13,139,143,1,10,143,147,1,2,147,151,1,6,151,0,99,2,14,0,0]
 
-def mod_four(i):
-    return (i % 4) == 0
+# https://riptutorial.com/python/example/2920/argument-passing-and-mutability
+def process_intcode(input):
+  opcodes = input.copy()
+  for address in range(len(opcodes)):
+    if ((address % 4) != 0): continue
+    instruction = opcodes[address]
 
-def toString(li):
-  return separator.join(map(str, li))
+    parameterA_address = opcodes[address + 1]
+    parameterB_address = opcodes[address + 2]
+    parameterC_address = opcodes[address + 3]
 
-opcodes = list(map(int, input))
-functions = filter(mod_four, range(len(opcodes)))
+    parameterA = opcodes[parameterA_address]
+    parameterB = opcodes[parameterB_address]
 
-for i in functions:
-  opcode = opcodes[i]
-  if (opcode == 99): break
+    if (instruction == 1): opcodes[parameterC_address] = parameterA + parameterB
+    elif (instruction == 2): opcodes[parameterC_address] = parameterA * parameterB
+    elif (instruction == 99): break
+  return opcodes
 
-  input1_index = opcodes[i+1]
-  input2_index = opcodes[i+2]
-  output_index = opcodes[i+3]
-
-  input1 = opcodes[input1_index]
-  input2 = opcodes[input2_index]
-
-  if (opcode == 1): opcodes[output_index] = input1 + input2
-  elif (opcode == 2): opcodes[output_index] = input1 * input2
-
-print(toString(opcodes))
+result = process_intcode(input)
+print(result)
